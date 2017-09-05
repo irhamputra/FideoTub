ac1a1682c2264da0eb9f856d51e828681a038a50
irhamputra <irhamputraprasetyo@gmail.com>
2017-09-05 14:29:47 +0200

    Throttling the searchbar value

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ class App extends Component{
     }
 
     render() {
-        const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
+        const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
         return (
             <div>
                 <SearchBar onSearchTermChange={videoSearch} />
